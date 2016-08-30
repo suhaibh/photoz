82cc6045b9f4257d9196ae345497f1afb79098ef
suhaibh <suhaib.habibullah@gmail.com>
2016-08-30 00:05:41 -0400

    Set up SendGrid for production and development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   # Default options for Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
